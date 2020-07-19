be9f5c372e4b799f0f0c067009b585fae1a16c42
Fayik99 <fayik99@gmail.com>
2020-07-20 02:33:14 +0530

    Color changes done

--- Airbnb-COBSCComp191P-035/ViewController.swift
+++ Airbnb-COBSCComp191P-035/ViewController.swift
@@ -13,15 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapButton))
-        self.navigationItem.leftBarButtonItem = addButton
+      /*  let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapButton))
+        self.navigationItem.leftBarButtonItem = addButton */
+        
+        let clsButton = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(tapButton))
+        
+        self.navigationItem.leftBarButtonItem = clsButton
         
         let login = UIBarButtonItem(title: "Login", style: .done, target: self, action: #selector(tapButton))
        
         self.navigationItem.rightBarButtonItem = login
         
         navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0, green: 0.6194828818, blue: 0.6715180838, alpha: 1)
-    }
+        navigationItem.rightBarButtonItem?.tintColor = UIColor.white
+
+      }
     @objc func tapButton() {
         print("You tap!!")
     }
