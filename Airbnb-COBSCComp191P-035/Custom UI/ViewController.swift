a6367b5d6d2996d68a917fc78e07d71cd0123f0e
Fayik99 <fayik99@gmail.com>
2020-07-26 21:08:03 +0530

    Constraint and UI changes

--- Airbnb-COBSCComp191P-035/Custom UI/ViewController.swift
+++ Airbnb-COBSCComp191P-035/Custom UI/ViewController.swift
@@ -15,22 +15,22 @@ class ViewController: UIViewController {
         
       /*  let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tapButton))
         self.navigationItem.leftBarButtonItem = addButton */
-        
-        let clsButton = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(tapButton))
-        
-        self.navigationItem.leftBarButtonItem = clsButton
-        
-        let login = UIBarButtonItem(title: "Login", style: .done, target: self, action: #selector(tapButton))
-       
-        self.navigationItem.rightBarButtonItem = login
-        
-        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0, green: 0.6194828818, blue: 0.6715180838, alpha: 1)
-        navigationItem.rightBarButtonItem?.tintColor = UIColor.white
-
-      }
-    @objc func tapButton() {
-        print("You tap!!")
-    }
+//
+//        let clsButton = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(tapButton))
+//
+//        self.navigationItem.leftBarButtonItem = clsButton
+//
+//        let login = UIBarButtonItem(title: "Login", style: .done, target: self, action: #selector(tapButton))
+//
+//        self.navigationItem.rightBarButtonItem = login
+//
+//        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0, green: 0.6194828818, blue: 0.6715180838, alpha: 1)
+//        navigationItem.rightBarButtonItem?.tintColor = UIColor.white
+//
+//      }
+//    @objc func tapButton() {
+//        print("You tap!!")
+//    }
 }
-
+}
     
